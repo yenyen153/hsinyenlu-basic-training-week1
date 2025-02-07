cf3f906c139790c4f2f7c032af09f275f9a8fd34
yenyen153 <live871008@gmail.com>
2025-02-07 16:50:10 +0800

    資料結構 - list

--- hsinyenlu-basic-training.py
+++ hsinyenlu-basic-training.py
@@ -130,3 +130,29 @@ common_token = sent1_set & sent2_set
 
 print(common_token)
 # >>>{'詞', '的', 'VP', '，', 'syntax', 'tree', ' ', '、', 'NP', '是'}
+
+#%%
+## 資料結構 - list
+## list是有順序且能對list中的元素增減，並允許重複元素存在
+## 銜接上面使用的例句，下面以有序的方式在list內添加詞彙做示範
+
+text_token.insert(text_token.index('書局') + 1, '電影院') # 可以使用insert去指定添加詞與位置
+print(text_token)
+# >>>['今天', '的', '天氣', '很', '好', '，', '我', '的', '心情', '也', '很', '好', '，', '下班', '後', '想', '去', '咖啡', '廳', '、', '書局', '電影院', '，', '但', '時間', '不夠', '，', '只好', '回家']
+
+## 刪除提示詞
+stop_words = {'我','的','但','，','也'}  # 可以使用list comprehension刪除停用詞
+token_without_stopwords = [words for words in text_token if words not in stop_words]
+print(token_without_stopwords)
+# >>>['今天', '天氣', '很', '好', '心情', '很', '好', '下班', '後', '想', '去', '咖啡', '廳', '、', '書局', '電影院', '時間', '不夠', '只好', '回家']
+
+## 找出詞頻
+text_freq = [text_token.count(freq) for freq in set(text_token)]
+print(text_freq)
+# >>>[1, 4, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1]
+
+## 可找出詞的順序
+## list有序的概念可以使語意更清楚，以下為例
+sent1 = "the cat sit on the table".split() # >>>['the', 'cat', 'sit', 'on', 'the', 'table']
+sent2 = "the tabel sit on the cat".split() # >>>['the', 'table', 'sit', 'on', 'the', 'cat']
+## 上述sent1,sent2的cat與table調換後，是完全不同意思
