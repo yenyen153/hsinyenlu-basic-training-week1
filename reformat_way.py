c62695cfb071347f051dd4588e4effaa5b89ccb2
yenyen153 <live871008@gmail.com>
2025-02-10 13:41:59 +0800

    reformat的方法

---
+++ reformat_way.py
@@ -0,0 +1,8 @@
+# %%
+## 快速reformat程式碼
+
+## reformat的原因 : .1 統一程式格式，便於開發、2. 避免格式錯誤
+
+## 方法 1 : file --> settings --> commit --> commit checks --> reformat code
+## 可以使每次commit時都自動reformat好
+## 方法 2 : shift + crtl + alt +　Ｌ鍵　--> 即可選擇是否reformat 
