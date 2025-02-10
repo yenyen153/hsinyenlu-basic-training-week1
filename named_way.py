7c62e0b191a71ffe8153765758fbd36aa2a117b0
yenyen153 <live871008@gmail.com>
2025-02-10 11:26:49 +0800

    package &  module命名規則

---
+++ named_way.py
@@ -0,0 +1,9 @@
+# %%
+## 命名規則
+## 命名規則的產生，是為了讓開發者都能有統一的命名方式，互相能快速了解變數、函式等功用
+
+# Package : 全小寫，避免 _ 、 特殊符號 --> tools
+# Module : 全小寫，可用 _ 分隔，避免和標準庫名稱重複 --> tool.py
+## Package & Module參考project資料夾展示 (至於Package & Module branch中)
+
+## 可以參考branch:Package&Module的檔案
