2892e6ef7e067e56d1c12c7086876812bc246e0a
呂芯妍 <hsinyenlu@MacBook-Air.local>
2025-03-07 17:59:14 +0800

    create board table control

---
+++ crud/author.py
@@ -0,0 +1,20 @@
+from app.models import *
+
+
+def get_author_by_ptt_id(db, author_ptt_id):
+    author = db.query(AuthorTable).filter_by(author_ptt_id=author_ptt_id).first()
+
+    return author
+
+
+def get_author_by_id(db, author_id):
+    author = db.query(AuthorTable).get(author_id)
+    return author
+
+def create_author(db, **ptt_post):
+    author = AuthorTable(
+        author_nickname=ptt_post['.author_nickname'],
+    )
+    db.add(author)
+    db.commit()
+    db.refresh(author)
---
+++ crud/board.py
@@ -0,0 +1,20 @@
+from app.models import *
+
+def get_board_by_board_name(db, board_name):
+    board = db.query(BoardTable).filter_by(board=board_name).first()
+
+    return board
+
+def get_board_by_id(db, board_id):
+    board = db.query(BoardTable).get(board_id)
+    return board
+
+
+def create_board(db, **ptt_post):
+    board = BoardTable(
+        board=ptt_post['board_name'],
+        url=f"https://www.ptt.cc/bbs/{ptt_post['board_name']}/index.html"
+    )
+    db.add(board)
+    db.commit()
+    db.refresh(board)
