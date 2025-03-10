5f812419c6778f5ad6f504cc06b5e195c6358238
呂芯妍 <hsinyenlu@MacBook-Air.local>
2025-03-10 17:18:13 +0800

    delte pttposttalbe use createposts to check data

--- app/schemas.py
+++ app/schemas.py
@@ -2,23 +2,6 @@ from pydantic import BaseModel, HttpUrl, field_validator, Field
 import re
 from datetime import datetime
 
-# class PttPostModel(BaseModel):
-#     board_name: str
-#     title: str
-#     link: HttpUrl
-#     author_ptt_id: str
-#     date: str
-#     author_nickname: str
-#     content: str
-#
-#     @field_validator('date')
-#     @classmethod
-#     def validate_date(cls, value:str) -> str:
-#         if not re.search(r"\d{4}[/]\d{2}[/]\d{2}\s\d{2}[:]\d{2}[:]\d{2}", value):
-#             raise ValueError('wrong date format')
-#         return value
-# 這邊 直接使用createPosts來創建
-
 class CreatePosts(BaseModel):
     board_name: str
     title: str
