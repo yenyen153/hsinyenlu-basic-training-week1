edf3a10a4c3a1189f577fecbf0e790d2f1f59a19
呂芯妍 <hsinyenlu@MacBook-Air.local>
2025-03-10 14:27:03 +0800

    add function check board

--- crud/board.py
+++ crud/board.py
@@ -18,3 +18,12 @@ def create_board(db, **ptt_post):
     db.add(board)
     db.commit()
     db.refresh(board)
+    return board
+
+
+def check_board(db, **ptt_post):
+    board = get_board_by_board_name(db, ptt_post['board_name'])
+    if not board:
+        board = create_board(db, **ptt_post)
+
+    return board
