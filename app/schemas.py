a6b38101e318b3dc47aa46b5dfd1689c33eb52cf
呂芯妍 <hsinyenlu@MacBook-Air.local>
2025-03-10 15:48:59 +0800

    schemas update

--- app/schemas.py
+++ app/schemas.py
@@ -2,28 +2,27 @@ from pydantic import BaseModel, HttpUrl, field_validator, Field
 import re
 from datetime import datetime
 
-# class PttPostModel(BaseModel):
-#     board_name: str
-#     title: str
-#     link: HttpUrl
-#     author_ptt_id: str
-#     date: str
-#     author_nickname: str
-#     content: str
-#
-#     @field_validator('date')
-#     @classmethod
-#     def validate_date(cls, value:str) -> str:
-#         if not re.search(r"\d{4}[/]\d{2}[/]\d{2}\s\d{2}[:]\d{2}[:]\d{2}", value):
-#             raise ValueError('wrong date format')
-#         return value
+class PttPostModel(BaseModel):
+    board_name: str
+    title: str
+    link: HttpUrl
+    author_ptt_id: str
+    date: str
+    author_nickname: str
+    content: str
+
+    @field_validator('date')
+    @classmethod
+    def validate_date(cls, value:str) -> str:
+        if not re.search(r"\d{4}[/]\d{2}[/]\d{2}\s\d{2}[:]\d{2}[:]\d{2}", value):
+            raise ValueError('wrong date format')
+        return value
 # 這邊 直接使用createPosts來創建
 
 class CreatePosts(BaseModel):
     board_name: str
     title: str
     link: HttpUrl
-    # link: Union[HttpUrl, str]
     author_ptt_id: str
     date: datetime
     author_nickname: str
@@ -37,7 +36,6 @@ class PostResponse(BaseModel):
     id: int
     title: str
     link: HttpUrl
-    # link: Union[HttpUrl, str]
     date: datetime
     board_id: int
     author_id: int
