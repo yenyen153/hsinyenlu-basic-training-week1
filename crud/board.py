2cb3ba111bd688b71fe5a0f82e1248aefaaa79f8
呂芯妍 <hsinyenlu@MacBook-Air.local>
2025-03-11 17:41:59 +0800

    clean

--- crud/board.py
+++ crud/board.py
@@ -6,7 +6,6 @@ def get_board_by_id(db, board_id):
     board = db.get(BoardTable,board_id)
     return board
 
-# Todo finish get_board_by_board_name and author (finish)
 def get_and_create_board(db, board_name,create_if_not_exists=False):
     board = db.query(BoardTable).filter_by(board=board_name).first()
     if not board:
