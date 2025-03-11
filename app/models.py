c6f7afb0404752664ece7dd24e51383cc8ac3470
呂芯妍 <hsinyenlu@MacBook-Air.local>
2025-03-11 17:41:04 +0800

    models update

--- app/models.py
+++ app/models.py
@@ -9,7 +9,7 @@ class PttPostsTable(Base):
     board_id = sa.Column(sa.Integer, sa.ForeignKey("board.id"), nullable=False)
     author_id = sa.Column(sa.Integer, sa.ForeignKey("author.id"), nullable=False)
     title = sa.Column(sa.String(100), nullable=False)
-    link = sa.Column(sa.String(255), unique=True, nullable=False)
+    link = sa.Column(sa.String(2048), unique=True, nullable=False)
     date = sa.Column(sa.String(20), nullable=False)
     content = sa.Column(sa.Text)
 
@@ -22,7 +22,7 @@ class BoardTable(Base):
 
     id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
     board = sa.Column(sa.String(60), unique=True, nullable=False)
-    url = sa.Column(sa.String(255), nullable=False)
+    url = sa.Column(sa.String(2048), nullable=False)
 
     posts = relationship("PttPostsTable", back_populates="board", foreign_keys=[PttPostsTable.board_id])
 
@@ -31,7 +31,7 @@ class AuthorTable(Base):
 
     id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
     author_ptt_id = sa.Column(sa.String(100), unique=True, nullable=False)
-    author_nickname = sa.Column(sa.String(100), nullable=False)
+    author_nickname = sa.Column(sa.String(100), nullable=True)
 
     posts = relationship("PttPostsTable", back_populates="author", foreign_keys=[PttPostsTable.author_id])
 
