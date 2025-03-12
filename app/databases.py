fba4ed26102a64c30eef9409e87f03b15f60d141
呂芯妍 <hsinyenlu@MacBook-Air.local>
2025-03-12 17:25:13 +0800

    nothing change

--- app/databases.py
+++ app/databases.py
@@ -2,7 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 
-
 engine = create_engine(
     "mysql+pymysql://user:password@localhost/ptt_db?charset=utf8mb4",
     connect_args={"charset": "utf8mb4"}
