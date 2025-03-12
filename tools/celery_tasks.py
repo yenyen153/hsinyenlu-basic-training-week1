c3d17dd6637e3c53e9c7b71e4b8a2db0b462ae1c
呂芯妍 <hsinyenlu@MacBook-Air.local>
2025-03-12 09:02:29 +0800

    celery tasks update

--- tools/celery_tasks.py
+++ tools/celery_tasks.py
@@ -6,7 +6,7 @@ from tools.crawler_tool import *
 from sqlalchemy import create_engine
 from app.schemas import *
 from crud.post import input_post
-from crud.crawler_log import log_in
+from crud.crawler_log import log_to_db
 from settings import settings
 
 
@@ -16,7 +16,7 @@ app = Celery("task",
 
 app.conf.beat_schedule = {
     "fetch_ptt_every_min": {
-        "task": "celery_tasks.test_crawler",
+        "task": "tools.celery_tasks.test_crawler",
         "schedule": crontab(minute='*/1')
     }
 }
@@ -34,28 +34,35 @@ logging.basicConfig(
 engine = create_engine(settings.DATABASE_URL)
 Session = sessionmaker(bind=engine)
 
+
 @app.task
-def run_crawler():
-    PTT_BOARDS = ['mobilecomm','C_Chat','Baseball','Lifeismoney','home-sale',"NBA"]
-    for board in PTT_BOARDS:
-        my_logger.info(f"正在爬取{board}")
+def test_crawler():
+    PTT_BOARDS = ['C_Chat','Baseball','Lifeismoney','home-sale',"NBA",'mobilecomm']
+    db = Session()
 
     for board in PTT_BOARDS:
+        log_message = f"正在爬取 {board}"
+        my_logger.info(log_message)
+        log_to_db(db, log_message)
+
         links = fetch_link(board)
         for link in links:
-            db = Session()
             try:
                 post = fetch_author(link)
                 post['date'] = datetime.strptime(post['date'], "%Y/%m/%d %H:%M:%S")
-
+                post['board_name'] = board
                 CreatePosts(**post)
                 input_post(db, **post)
                 db.commit()
+
+                log_message = f"成功插入文章: {post['title']}"
+                my_logger.info(log_message)
+                log_to_db(db, log_message)
+
             except Exception as e:
                 db.rollback()
-                my_logger.error(f"插入失敗: {str(e)}")
+                error_message = f"插入失敗: {str(e)}"
+                my_logger.error(error_message)
+                log_to_db(db, error_message)
             finally:
                 db.close()
-
-
-    log_in(Session,'crawler.log')
